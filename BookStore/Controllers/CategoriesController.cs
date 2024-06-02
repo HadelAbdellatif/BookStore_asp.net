6fe052b6d5ddccea2478982db79aabf879239e91
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2024-06-02 23:49:26 +0300

    Show Categories

---
+++ BookStore/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = context.categories.ToList();
+            return View("Index", categories);
+        }
+    }
+}
